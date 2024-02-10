4170e31a4c1883b154a978662242709e8f8b023b
Sumaita Binte Shorif <51522304+SumaitaB@users.noreply.github.com>
2024-02-10 23:31:17 +0600

    search testcase for tests.py

--- BharaHobe/rentapp/tests.py
+++ BharaHobe/rentapp/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Product
+from .forms import SearchForm
 
-# Create your tests here.
+class ProductSearchTestCase(TestCase):
+    def setUp(self):
+        # Create some sample products for testing
+        Product.objects.create(name='Product 1', description='Description 1')
+        Product.objects.create(name='Product 2', description='Description 2')
+        Product.objects.create(name='Product 3', description='Description 3')
+
+    def test_search_form_rendered_correctly(self):
+        client = Client()
+        response = client.get(reverse('search'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.context['form'], SearchForm)
+
+    def test_search_results(self):
+        client = Client()
+        response = client.get(reverse('search_results'), {'query': 'Product 1'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Product 1')  # Ensure that 'Product 1' is present in the response content
+        self.assertNotContains(response, 'Product 2')  # Ensure that 'Product 2' is not present in the response content
