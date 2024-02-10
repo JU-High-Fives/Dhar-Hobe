f3a7201c2c621251e0c4fd29854522599cd7d8f4
jannat-349 <jucse28.349@gmail.com>
2024-02-10 18:49:47 +0600

    Test a valid form submission in the make_payment view and it failed by Jannat

--- BharaHobe/payment/tests.py
+++ BharaHobe/payment/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
+from .models import PaymentModel
 
 class PaymentViewTests(TestCase):
     def test_make_payment_view(self):
@@ -13,3 +14,20 @@ class PaymentViewTests(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'make_payment.html')
+    
+    def test_make_payment_form_valid_submission(self):
+        """
+        Test a valid form submission in the make_payment view.
+
+        Checks that the form submission redirects to 'payment_success.html' and creates a new payment instance.
+        """
+        url = reverse('make_payment')
+        data = {'order_id': '123', 'amount': 100.00, 'success': True}
+        response = self.client.post(url, data)
+
+        self.assertRedirects(response, reverse('payment_success'))
+        self.assertEqual(PaymentModel.objects.count(), 1)
+        payment = PaymentModel.objects.first()
+        self.assertEqual(payment.m_order_id, '123')
+        self.assertEqual(payment.m_amount, 100.00)
+        self.assertTrue(payment.m_success)
