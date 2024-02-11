53e01bd7ce73ad74bcf39317a56a19291c43d2e7
Sumaita Binte Shorif <51522304+SumaitaB@users.noreply.github.com>
2024-02-11 20:14:11 +0600

    Created product model

--- BharaHobe/rentapp/models.py
+++ BharaHobe/rentapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    location = models.CharField(max_length=100)
+    # Add more fields as needed
