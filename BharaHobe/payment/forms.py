edae41729fd617e6f2d8da3e2db56c6baf989a10
jannat-349 <jucse28.349@gmail.com>
2024-02-11 19:48:02 +0600

    Removed previous payment form code

--- BharaHobe/payment/forms.py
+++ BharaHobe/payment/forms.py
@@ -1,10 +1,5 @@
 from django import forms
 
-class paymentForm(forms.Form):
-    f_order_id = forms.CharField(label='Order ID', required= True)
-    f_amount = forms.DecimalField(label='Amount', min_value=0.01, required= True)
-    f_isSuccess = forms.BooleanField(label='Success', required=False)
-
 class advancePaymentForm(forms.Form):
     f_order_id = forms.CharField(label='Order ID', required= True)
     f_amount = forms.DecimalField(label='Advance amount', min_value=0.01, required= True)
