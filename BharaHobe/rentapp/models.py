1eb42a39321a113e1bd10d71eacd8f30392c80f3
Sumaita Binte Shorif <51522304+SumaitaB@users.noreply.github.com>
2024-02-13 21:18:29 +0600

    Added test methods to test specific aspects of the Product

--- BharaHobe/rentapp/models.py
+++ BharaHobe/rentapp/models.py
@@ -6,3 +6,9 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     location = models.CharField(max_length=100)
     # Add more fields as needed
+
+    def __str__(self):
+        return self.name
+    
+    # We write test methods to test specific aspects of the Product model, such as its __str__ method and attribute values.
+
