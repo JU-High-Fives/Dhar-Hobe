335d470925c5a3012af1f649499ea2c0f31f1843
jannat-349 <jucse28.349@gmail.com>
2024-02-16 12:23:24 +0600

    modified the test case

--- BharaHobe/payment/test_advance_payment.py
+++ BharaHobe/payment/test_advance_payment.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
+from django.http import HttpResponseBadRequest
 from .forms import advancePaymentForm
+from .views import advance_payment_view
 from django.core.exceptions import ValidationError
+from .models import OrderModel, PaymentModel
 
 
 class TestAdvancePaymentForm(TestCase):
@@ -25,41 +28,36 @@ class TestAdvancePaymentForm(TestCase):
         """Test form validation for a valid credit card."""
         form = advancePaymentForm(data=self.valid_credit_card_data)
         with self.assertRaises(ValidationError) as context:
-            form.full_clean()
+            advancePaymentForm(data=form).full_clean()
 
         expected_error_message = 'Invalid credit card number'
         self.assertEqual(str(context.exception), expected_error_message)
 
     def test_invalid_credit_card(self):
         """Test form validation for an invalid credit card."""
-        form = advancePaymentForm(data=self.invalid_credit_card_data)
         with self.assertRaises(ValidationError) as context:
-            form.full_clean()
+            advancePaymentForm(data=self.invalid_credit_card_data).full_clean()
 
         expected_error_message = 'Invalid credit card number'
         self.assertEqual(str(context.exception), expected_error_message)
 
-    def test_no_duplicate_payment(self):
-        """Test that no duplicate payment is allowed."""
-        form_data = {
-            'f_payment_method': 'paypal',
-            'f_credit_card_number': '4111125634596325',
-            'f_notes': 'Test notes',
-            'f_amount': 100.00,
-            'f_card_token': '',
-        }
+    def test_duplicate_payment(self):
+        """Test for attempting to make a duplicate payment."""
+        order = OrderModel.objects.create(
+            m_order_id='123',
+            m_items='Test items',
+            m_total_amount=200.00,
+            m_notes='Test order notes'
+        )
 
-        form = advancePaymentForm(data=form_data)
-        form.full_clean()
+        PaymentModel.objects.create(
+            m_order_id=order.m_order_id,
+            m_amount=100.00,
+            m_isSuccess=True,
+            m_payment_method='credit_card',
+            m_notes='Test payment notes',
+            m_card_token='mock_card_token'
+        )
 
-        with self.assertRaises(ValidationError) as context:
-            form = advancePaymentForm(data=form_data)
-            form.full_clean()
-
-        expected_error_message = 'Payment with this Credit Card/PayPal Number already exists.'
-        self.assertEqual(str(context.exception), expected_error_message)
-
-
-if __name__ == '__main__':
-    import unittest
-    unittest.main()
+        request = self.client.post('/advance_payment_form/<int:order_id>/', data=self.valid_credit_card_data)
+        self.assertEqual(request.status_code, HttpResponseBadRequest.status_code)
