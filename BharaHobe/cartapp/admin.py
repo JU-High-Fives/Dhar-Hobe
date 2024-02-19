193b0fa285faedc24401cd69d4a5deb666781e25
Sumaita Binte Shorif <51522304+SumaitaB@users.noreply.github.com>
2024-02-19 21:11:48 +0600

    edit changes

--- BharaHobe/cartapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import PaymentModel
-
-@admin.register(PaymentModel)
-class PaymentModelAdmin(admin.ModelAdmin):
-    search_fields = ['m_order_id']  # Specify the field(s) you want to enable search on
--- BharaHobe/cartapp/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class SearchForm(forms.Form):
-    query = forms.CharField(label='Search Query', required=True)
