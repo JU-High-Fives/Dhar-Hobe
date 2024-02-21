cc8ad1e5a4c3992487a1dcac0d90c69b850ed6ef
Sumaita Binte Shorif <51522304+SumaitaB@users.noreply.github.com>
2024-02-21 20:35:42 +0600

    Update urls.py to resolve API endpoints issue

--- BharaHobe/cartapp/urls.py
+++ BharaHobe/cartapp/urls.py
@@ -5,3 +5,13 @@ urlpatterns = [
     path('search/', search_view, name='search'),  # URL to render the search form
     path('search-results/', search_results_view, name='search_results'),  # URL to handle search results
 ]
+
+# cartapp/urls.py
+
+from django.urls import path
+from .views import add_to_cart, get_cart_details
+
+urlpatterns = [
+    path('add-to-cart/', add_to_cart, name='add_to_cart'),
+    path('cart/', get_cart_details, name='get_cart_details'),
+]
