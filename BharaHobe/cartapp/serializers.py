8bbee38306fd8ce76d9d291cf847816debfdec69
Sumaita Binte Shorif <51522304+SumaitaB@users.noreply.github.com>
2024-02-21 20:36:26 +0600

    Update serializers.py

--- BharaHobe/cartapp/serializers.py
+++ BharaHobe/cartapp/serializers.py
@@ -1,1 +1,17 @@
+# cartapp/serializers.py
+
+from rest_framework import serializers
+from .models import Cart, CartItem
+
+class CartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = ['id', 'product', 'quantity']
+
+class CartSerializer(serializers.ModelSerializer):
+    cart_items = CartItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Cart
+        fields = ['id', 'user', 'created_at', 'cart_items']
 
