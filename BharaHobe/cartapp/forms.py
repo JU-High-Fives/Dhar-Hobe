263ad2d3dfce249f46cd8011cdae19a418cbc1d3
Sumaita Binte Shorif <51522304+SumaitaB@users.noreply.github.com>
2024-02-24 17:48:53 +0600

    Update forms.py and added documentation

--- BharaHobe/cartapp/forms.py
+++ BharaHobe/cartapp/forms.py
@@ -1,4 +1,13 @@
 from django import forms
 
-class SearchForm(forms.Form):
-    query = forms.CharField(label='Search Query', required=True)
+class CartItemForm(forms.Form):
+    """
+    Form class for adding items to the cart.
+
+    Attributes:
+        product_id (IntegerField): The ID of the product to add to the cart.
+        quantity (IntegerField): The quantity of the product to add to the cart.
+    """
+
+    product_id = forms.IntegerField()
+    quantity = forms.IntegerField(min_value=1)
