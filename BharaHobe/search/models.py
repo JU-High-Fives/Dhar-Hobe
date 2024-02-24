de18180f215c591a7994e857cc9583127d9d4578
Sumaita Binte Shorif <51522304+SumaitaB@users.noreply.github.com>
2024-02-24 17:43:52 +0600

    Update models.py

--- BharaHobe/search/models.py
+++ BharaHobe/search/models.py
@@ -1,15 +1,28 @@
 from django.db import models
 
-class PaymentModel(models.Model):
-    m_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    m_isSuccess = models.BooleanField(default=False)
-    m_order_id = models.CharField(max_length = 10, default = "")
-    m_timestamp = models.DateTimeField(auto_now_add=True, verbose_name='Timestamp')
+class Product(models.Model):
+    """
+    Model representing a product.
+
+    Attributes:
+        name (str): The name of the product.
+        category (str): The category of the product.
+        price (Decimal): The price of the product.
+        location (str): The location where the product is located.
+        description (str): A description of the product.
+    """
+
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    location = models.CharField(max_length=100)
+    description = models.TextField()
 
     def __str__(self):
-        """This functions returns the payment id 
+        """
+        Returns a string representation of the product.
 
         Returns:
-            int: Payment ID 
+            str: The name of the product.
         """
-        return f"Payment #{self.id}"
+        return self.name
