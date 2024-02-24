3e6be6e3108d7e11dc068cc8832e9755555f6a30
SumaitaB <sumaita.bs@gmail.com>
2024-02-25 02:34:03 +0600

    update product model

--- BharaHobe/rentapp/models.py
+++ BharaHobe/rentapp/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
+from django.db import models
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    category = models.CharField(max_length=50)  # Add this line for a CharField
+    # OR
+    # category = models.ForeignKey(Category, on_delete=models.CASCADE)  # Add this line for a ForeignKey
 
-    # Add more fields as needed
+    # Add other fields as needed
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    location = models.CharField(max_length=100)
+
 
     def __str__(self):
         return self.name
