90cda739a4c713c75f2752ebb9d00139192c589d
Sumaita Binte Shorif <51522304+SumaitaB@users.noreply.github.com>
2024-02-24 17:41:21 +0600

    Update apps.py added documentation

--- BharaHobe/search/apps.py
+++ BharaHobe/search/apps.py
@@ -1,6 +1,17 @@
+# apps.py
 from django.apps import AppConfig
 
+class SearchConfig(AppConfig):
+    """
+    AppConfig for the search app.
 
-class PaymentConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'payment'
+    This class defines the configuration for the search app, including its
+    name and verbose name.
+
+    Attributes:
+        name (str): The name of the app.
+        verbose_name (str): The human-readable name of the app.
+    """
+
+    name = 'search'
+    verbose_name = 'Search'
