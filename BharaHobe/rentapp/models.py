055db9fd957e8491e9d08c9178ed6f675bc9f06e
SumaitaB <sumaita.bs@gmail.com>
2024-02-25 02:17:15 +0600

    Add product model to rentapp

--- BharaHobe/rentapp/models.py
+++ BharaHobe/rentapp/models.py
@@ -2,10 +2,9 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    category = models.CharField(max_length=50)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    location = models.CharField(max_length=100)
     description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
     # Add more fields as needed
 
     def __str__(self):
