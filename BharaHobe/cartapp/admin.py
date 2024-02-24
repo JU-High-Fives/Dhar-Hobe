9f06b0c69bc93aeb74b9a74a36347204524d279a
Sumaita Binte Shorif <51522304+SumaitaB@users.noreply.github.com>
2024-02-24 17:47:18 +0600

    Update admin.py with documentation

--- BharaHobe/cartapp/admin.py
+++ BharaHobe/cartapp/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
-from .models import PaymentModel
+from .models import Cart, CartItem
 
-@admin.register(PaymentModel)
-class PaymentModelAdmin(admin.ModelAdmin):
-    search_fields = ['m_order_id']  # Specify the field(s) you want to enable search on
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Cart model.
+
+    Attributes:
+        list_display (tuple): The fields to display in the admin list view.
+    """
+
+    list_display = ('user', 'created_at')
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the CartItem model.
+
+    Attributes:
+        list_display (tuple): The fields to display in the admin list view.
+    """
+
+    list_display = ('cart', 'product', 'quantity')
