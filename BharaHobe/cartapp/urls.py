ba9cebc6329139daed3acfb4fb986b410287d9ab
Sumaita Binte Shorif <51522304+SumaitaB@users.noreply.github.com>
2024-02-24 17:54:19 +0600

    Update urls.py and documentation added

--- BharaHobe/cartapp/urls.py
+++ BharaHobe/cartapp/urls.py
@@ -1,11 +1,3 @@
-from django.urls import path
-from .views import search_view, search_results_view
-
-urlpatterns = [
-    path('search/', search_view, name='search'),  # URL to render the search form
-    path('search-results/', search_results_view, name='search_results'),  # URL to handle search results
-]
-
 # cartapp/urls.py
 
 from django.urls import path
@@ -15,3 +7,9 @@ urlpatterns = [
     path('add-to-cart/', add_to_cart, name='add_to_cart'),
     path('cart/', get_cart_details, name='get_cart_details'),
 ]
+"""
+URL patterns for cart-related functionality.
+
+- /add-to-cart/: This endpoint allows users to add items to the cart.
+- /cart/: This endpoint retrieves the details of the user's cart, including the items added to it.
+"""
