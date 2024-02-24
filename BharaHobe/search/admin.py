255b887964507dae2bf5fdd173c1ab404c5d2d9c
Sumaita Binte Shorif <51522304+SumaitaB@users.noreply.github.com>
2024-02-24 17:40:07 +0600

    added documentation to admin.py for search feature

--- BharaHobe/search/admin.py
+++ BharaHobe/search/admin.py
@@ -1,6 +1,19 @@
+# admin.py
 from django.contrib import admin
-from .models import PaymentModel
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Product model.
 
-admin.site.register(PaymentModel)
+    This class defines the appearance and functionality of the Product model
+    in the Django admin interface.
+
+    Attributes:
+        list_display (tuple): Fields to display in the list view of the admin.
+        search_fields (tuple): Fields to enable search functionality in the admin.
+    """
+
+    list_display = ('name', 'category', 'price')
+    search_fields = ('name', 'category')
