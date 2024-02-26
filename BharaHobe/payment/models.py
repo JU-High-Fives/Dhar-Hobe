b0cb37c9280c85571deb4f5d79fe212044578789
jannat-349 <jucse28.349@gmail.com>
2024-02-26 22:59:44 +0600

    added documentation

--- BharaHobe/payment/models.py
+++ BharaHobe/payment/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 
 class OrderModel(models.Model):
+    """
+    Model representing an order.
+    """
     m_order_id = models.AutoField(primary_key=True)
     m_items = models.TextField()
     m_total_amount = models.DecimalField(max_digits=10, decimal_places=2)
     m_notes = models.TextField(blank=True, null=True)
 
     def __init__(self, *args, **kwargs):
+        """
+        Constructor for OrderModel.
+
+        Initializes the monthly payment amount based on the total amount.
+        """
         super(OrderModel, self).__init__(*args, **kwargs)
         if self.m_total_amount:
             self.m_monthly_pay = self.m_total_amount // 6
@@ -14,9 +22,15 @@ class OrderModel(models.Model):
             self.m_monthly_pay = 0
 
     def __str__(self):
+        """
+        Returns a string representation of the order.
+        """
         return f"Order #{self.m_order_id}"
 
 class PaymentModel(models.Model):
+    """
+    Model representing a payment for an order.
+    """
     m_order_id = models.CharField(max_length=50)
     m_amount = models.DecimalField(max_digits=10, decimal_places=2)
     m_isSuccess = models.BooleanField(default=False)
@@ -24,6 +38,8 @@ class PaymentModel(models.Model):
     m_notes = models.TextField(blank=True, null=True)
     m_card_token = models.CharField(max_length=50, blank=True, null=True)
 
-
     def __str__(self):
+        """
+        Returns a string representation of the payment.
+        """
         return f"Payment #{self.id}"
